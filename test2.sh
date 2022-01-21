5ddaaa7d96b33d9cad59ce08b8a110842cdc95c9
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-01-21 12:28:49 +0100

    mod. bone test

--- test2.sh
+++ test2.sh
@@ -1,1 +1,1 @@
-gcc -Wall -Werror -Wextra -D BUFFER_SIZE=32 main2.c ../get_next_bone.c ../get_next_bone_utils.c -fsanitize=address && ./a.out $1 $2
+gcc -Wall -Werror -Wextra -D BUFFER_SIZE=32 main2.c ../get_next_line.c ../get_next_line_utils_bonus.c -fsanitize=address && ./a.out $1 $2
