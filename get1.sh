5bbfa82a3d1148960b1dccddbbbe4fa1f4d881b0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-02-03 13:48:07 +0100

    crucial mod

--- get1.sh
+++ get1.sh
@@ -1,1 +1,1 @@
-gcc -Wall -Werror -Wextra -D BUFFER_SIZE=16 main1.c ../get_next_line.c ../get_next_line_utils.c -fsanitize=address && ./a.out $1 $2
+gcc -Wall -Werror -Wextra -D BUFFER_SIZE=16 gmain1.c ../get_next_line.c ../get_next_line_utils.c -fsanitize=address && ./a.out $1 $2
--- get2.sh
+++ get2.sh
@@ -1,1 +1,1 @@
-gcc -Wall -Werror -Wextra -D BUFFER_SIZE=32 main2.c ../*bonus*.c -fsanitize=address && ./a.out $1 $2
+gcc -Wall -Werror -Wextra -D BUFFER_SIZE=32 gmain2.c ../*bonus*.c -fsanitize=address && ./a.out $1 $2
